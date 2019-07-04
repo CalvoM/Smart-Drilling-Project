fd79e48d4982d8c6ebf138265975645063e17d63
CalvoM <calvomaks@gmail.com>
2019-07-04 16:25:26 +0300

    Add keypad trial code
    Keypad is in project to input measurement

--- MyCode.ino
+++ MyCode.ino
@@ -1,73 +1,108 @@
-#include <Wire.h>
-#include <hd44780.h>
-#include <hd44780ioClass/hd44780_I2Cexp.h>
+/*
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+*/
+#define S0 6
+#define S1 7
+#define S2 8
+#define S3 9
+#define OUT 10
+// include the library code:
+#include <LiquidCrystal.h>
+int redFrequency = 0;
+int greenFrequency = 0;
+int blueFrequency = 0;
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-# define STEP1 53
-//# define STEP2 49
-
-# define DIR1 52
-//# define DIR2 46
-
-# define MS1_1 48
-# define MS2_1 50
-# define MS3_1 51
+void setup() {
+  // set up the LCD's number of columns and rows:
+  //
+  pinMode(S0,OUTPUT);
+  pinMode(S1,OUTPUT);
+  pinMode(S2,OUTPUT);
+  pinMode(S3,OUTPUT);
+  //
+  pinMode(OUT,INPUT);
+  //
+  digitalWrite(S0,HIGH);
+  digitalWrite(S1,HIGH);
+  //
+  Serial.begin(9600);
+  
+  lcd.begin(20,4);
  
-//# define MS1_2 47
-//# define MS2_2 44
-//# define MS3_2 45
-
-# define LSW_1 2
-# define LSW_2 3
-
-hd44780_I2Cexp lcd;
-const int LCD_COLS = 20;
-const int LCD_ROWS = 4;
-int status;
-bool first_run = true;
-void setup(){
-  //Set up for the first a4988 chip
-  pinMode(STEP1,OUTPUT);
-  pinMode(13,OUTPUT);
-  pinMode(DIR1,OUTPUT);
-  pinMode(MS1_1,OUTPUT);
-  pinMode(MS2_1,OUTPUT);
-  pinMode(MS3_1,OUTPUT);
-  //Set up for the second a4988 chip
-//  pinMode(STEP2,OUTPUT);
-//  pinMode(DIR2,OUTPUT);
-//  pinMode(MS1_2,OUTPUT);
-//  pinMode(MS2_2,OUTPUT);
-//  pinMode(MS3_2,OUTPUT);
-  //Limit switches
-  pinMode(LSW_1,INPUT);
-  pinMode(LSW_2,INPUT);
-  // Setting the step to 1/16th
-  digitalWrite(MS1_1,HIGH);
-  digitalWrite(MS2_1,HIGH);
-  digitalWrite(MS3_1,HIGH);
-//  digitalWrite(MS1_2,HIGH);
-//  digitalWrite(MS2_2,HIGH);
-//  digitalWrite(MS3_2,HIGH);
-  //LCD config
-  status = lcd.begin(LCD_COLS,LCD_ROWS);
-  if(status){
-    status = -status;
-    hd44780::fatalError(status);
-  }
-  lcd.print("SMART DRILL");
+  // Print a message to the LCD.
+  
+  lcd.setCursor(0,1);
+  lcd.print("EN292-0613/2014");
+  lcd.setCursor(0,2);
+  lcd.print("EN292-0599/2014");
+  lcd.setCursor(0,3);
+  lcd.print("BEGINNING..");
+  delay(2000);
+  lcd.clear();
+  lcd.print("COLOR DETAILS");
 }
-void loop(){
-  digitalWrite(DIR1,HIGH);
-//  digitalWrite(DIR2,HIGH);
-while(true){
-  digitalWrite(13,HIGH);
-    digitalWrite(STEP1,HIGH);
-//    digitalWrite(STEP2,HIGH);
-    delayMicroseconds(50);
-    digitalWrite(STEP1,LOW);
-//    digitalWrite(STEP2,LOW);
-    delayMicroseconds(50);
-    digitalWrite(DIR1,LOW);
-    delayMicroseconds(50);
-}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+//  lcd.setCursor(0, 1);
+//  // print the number of seconds since reset:
+//  lcd.print(millis() / 1000);
+digitalWrite(S2,LOW);
+  digitalWrite(S3,LOW);
+  
+  // Reading the output frequency
+  redFrequency = pulseIn(OUT, LOW);
+  
+   // Printing the RED (R) value
+  lcd.setCursor(0,1);
+  lcd.print("R = ");
+  lcd.setCursor(2,1);
+  lcd.print(redFrequency);
+  delay(100);
+ 
+  
+  // Setting GREEN (G) filtered photodiodes to be read
+  digitalWrite(S2,HIGH);
+  digitalWrite(S3,HIGH);
+  
+  // Reading the output frequency
+  greenFrequency = pulseIn(OUT, LOW);
+  
+  // Printing the GREEN (G) value  
+  lcd.setCursor(0,2);
+  lcd.print("G = ");
+  lcd.setCursor(2,2);
+  lcd.print(greenFrequency);
+  delay(100);
+ 
+  // Setting BLUE (B) filtered photodiodes to be read
+  digitalWrite(S2,LOW);
+  digitalWrite(S3,HIGH);
+  
+  // Reading the output frequency
+  blueFrequency = pulseIn(OUT, LOW);
+  
+  // Printing the BLUE (B) value 
+   lcd.setCursor(0,3);
+  lcd.print("B = ");
+  lcd.setCursor(2,3);
+  lcd.print(blueFrequency);
+  delay(100);
+ 
 }
---
+++ getMesurements.ino
@@ -0,0 +1,55 @@
+void getMeasure(){
+  if(start){
+    lcd.setCursor(0,0);
+  lcd.print("Enter Length:");
+  char customKey = customKeypad.getKey();  
+  if (customKey){
+    lcd.print(customKey);
+    len +=customKey;
+    while(len_check<2){
+      customKey = customKeypad.getKey();
+      if(customKey){
+        lcd.print(customKey);
+        len_check++;
+        len +=customKey;
+      }
+    }
+    lenReady=true;
+    start = false;
+    len_check = 0;
+    ilen = len.toInt();
+    Serial.println(ilen);
+  }
+    }
+   //When length obtained
+  if(lenReady){
+    lcd.setCursor(0,1);
+    lcd.print("Enter Width :");
+    char customKey = customKeypad.getKey();
+      for(int i =0;i<1000;i++){
+    
+  }
+    if (customKey){
+      lcd.print(customKey);
+      width +=customKey;
+      while(len_check<2){
+        customKey = customKeypad.getKey();
+        if(customKey){
+          lcd.print(customKey);
+          len_check++;
+          width +=customKey;
+        }
+      }
+      widReady = true;
+    }
+  }
+  //When width obtained
+  if(widReady){
+  
+  iwidth = width.toInt();
+  lcd.setCursor(0,2);
+  lcd.print(ilen);
+  lcd.setCursor(0,3);
+  lcd.print(iwidth);
+  }
+}
